a3c1389c27008efd47631c2fa10625ac865b25b1
metalex9 <alexbainter@gmail.com>
2019-02-21 22:38:32 -0600

    Add piece

---
+++ packages/piece-impact/src/arpeggiate-once.js
@@ -0,0 +1,10 @@
+import getRandomBetween from './get-random-between';
+
+const arpeggiateOnce = ({ instrument, notes, withinTime, velocity = 1 }) => {
+  notes.forEach(note => {
+    const time = getRandomBetween(0, withinTime);
+    instrument.triggerAttack(note, `+${time}`, velocity);
+  });
+};
+
+export default arpeggiateOnce;
