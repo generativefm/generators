bc107de13738c7648bb3e031d4d9a5163e61b3a2
metalex9 <alexbainter@gmail.com>
2019-03-12 23:49:44 -0500

    Move building and linting process to top level

---
+++ rollup.config.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const glob = require('glob');
+const babel = require('rollup-plugin-babel');
+const json = require('rollup-plugin-json');
+
+const globPromise = (pattern, options) =>
+  new Promise((resolve, reject) =>
+    glob(pattern, options, (err, result) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(result);
+    })
+  );
+
+const makeConfig = (
+  dirname,
+  dependencies = {},
+  peerDependencies = {},
+  format
+) => ({
+  input: `${dirname}/src/piece.js`,
+  output: {
+    file: `${dirname}/dist/${format}.js`,
+    format,
+  },
+  external: [
+    '@generative-music/samples.generative.fm/browser-client',
+    'rxjs/operators',
+  ]
+    .concat(Reflect.ownKeys(dependencies))
+    .concat(Reflect.ownKeys(peerDependencies)),
+  plugins: [json(), babel({ exclude: 'node_modules/**' })],
+});
+
+const configsPromise = globPromise('./packages/piece-*').then(dirnames =>
+  dirnames.reduce((buildConfigs, dirname) => {
+    const {
+      dependencies,
+      peerDependencies,
+      //eslint-disable-next-line global-require
+    } = require(`${dirname}/package.json`);
+    return buildConfigs.concat(
+      ['esm', 'cjs'].map(format =>
+        makeConfig(dirname, dependencies, peerDependencies, format)
+      )
+    );
+  }, [])
+);
+
+module.exports = configsPromise;
