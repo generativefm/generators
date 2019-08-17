16fad942593a047413119227ac597f61c22451ac
metalex9 <alexbainter@gmail.com>
2019-08-17 13:15:25 -0500

    Prep piece for release

--- packages/piece-last-transit/src/piece.js
+++ packages/piece-last-transit/src/piece.js
@@ -17,41 +17,40 @@ const makePiece = ({
       if (Tone.context !== audioContext) {
         Tone.setContext(audioContext);
       }
-      return getBuffer(samples['idling-truck'][preferredFormat][0]).then(
-        buffer => {
-          const activeSources = [];
-          const vol = new Tone.Volume(15).connect(destination);
-          const reverb = new Tone.Reverb(5).set({ wet: 0.5 }).connect(vol);
-          const filter = new Tone.AutoFilter(Math.random() / 30).connect(
-            reverb
+      return Promise.all([
+        getBuffer(samples['idling-truck'][preferredFormat][0]),
+        new Tone.Reverb(5).set({ wet: 0.5 }).generate(),
+      ]).then(([buffer, reverb]) => {
+        const activeSources = [];
+        const vol = new Tone.Volume(15).connect(destination);
+        reverb.connect(vol);
+        const filter = new Tone.AutoFilter(Math.random() / 30).connect(reverb);
+        filter.start();
+        const lfo = new Tone.LFO(Math.random() / 100, 0.05, 0.25);
+        lfo.start();
+        const play = () => {
+          const source = new Tone.BufferSource(buffer)
+            .set({
+              onended: () => {
+                const i = activeSources.indexOf(source);
+                if (i >= 0) {
+                  activeSources.splice(i, 1);
+                }
+              },
+            })
+            .connect(filter);
+          lfo.connect(source.playbackRate);
+          source.start();
+          Tone.Transport.scheduleOnce(() => {
+            play();
+          }, `+${buffer.duration / 0.25 - Math.random()}`);
+        };
+        play();
+        return () =>
+          [buffer, vol, reverb, filter, lfo, ...activeSources].forEach(node =>
+            node.dispose()
           );
-          filter.start();
-          reverb.generate();
-          const lfo = new Tone.LFO(Math.random() / 100, 0.05, 0.25);
-          lfo.start();
-          const play = () => {
-            const source = new Tone.BufferSource(buffer)
-              .set({
-                onended: () => {
-                  const i = activeSources.indexOf(source);
-                  if (i >= 0) {
-                    activeSources.splice(i, 1);
-                  }
-                },
-              })
-              .connect(filter);
-            lfo.connect(source.playbackRate);
-            source.start();
-            Tone.Transport.scheduleOnce(() => {
-              play();
-            }, `+${buffer.duration / 0.25 - Math.random()}`);
-          };
-          play();
-          return [buffer, vol, reverb, filter, lfo, ...activeSources].forEach(
-            node => node.dispose()
-          );
-        }
-      );
+      });
     }
   );
 
