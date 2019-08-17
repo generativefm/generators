9db2c2eb1be072ccdb778a0a899668941a6f600e
metalex9 <alexbainter@gmail.com>
2019-08-17 13:16:05 -0500

    Add script for updating all manifests

---
+++ scripts/update-manifest.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const fs = require('fs').promises;
+const glob = require('glob');
+
+const filenames = glob.sync('./packages/*/*.gfm.manifest.json');
+
+Promise.all(
+  filenames.map(filename =>
+    fs
+      .readFile(filename, 'utf8')
+      .then(data =>
+        fs.writeFile(
+          filename,
+          JSON.stringify(
+            Object.assign({ test: true }, JSON.parse(data)),
+            null,
+            2
+          )
+        )
+      )
+  )
+).then(() => {
+  console.log('done');
+});
