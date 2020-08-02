dea13e366cbff9bcce7c6537228af4b475065c7a
metalex9 <alexbainter@gmail.com>
2020-08-02 14:30:58 -0500

    aisatsana: update to new interface

--- packages/piece-aisatsana/src/piece.js
+++ packages/piece-aisatsana/src/piece.js
@@ -1,6 +1,6 @@
 import Chain from 'markov-chains';
-import Tone from 'tone';
-import { getSampler } from '@generative-music/utilities';
+import * as Tone from 'tone';
+import { getSampler, makePiece } from '@generative-music/utilities';
 import instructions from './instructions.json';
 
 const BPM = 102;
@@ -11,61 +11,67 @@ const EIGHTH_NOTE_INTERVAL_S =
 const DELIMITER = ',';
 const SONG_LENGTH = 301;
 
-const notes = instructions.tracks[1].notes.slice(0);
-const eighthNotes = [];
-
-for (let time = 0; time <= SONG_LENGTH; time += EIGHTH_NOTE_INTERVAL_S) {
-  const names = notes
-    .filter(
-      note => time <= note.time && note.time < time + EIGHTH_NOTE_INTERVAL_S
-    )
-    .map(({ name }) => name)
-    .sort();
-  eighthNotes.push(names.join(DELIMITER));
-}
-
-const phrases = [];
-const phraseLength = 32;
-const enCopy = eighthNotes.slice(0);
-while (enCopy.length > 0) {
-  phrases.push(enCopy.splice(0, phraseLength));
-}
-
-const phrasesWithIndex = phrases.map(phrase =>
-  phrase.map((names, i) =>
-    names.length === 0 ? `${i}` : `${i}${DELIMITER}${names}`
-  )
-);
-
-const chain = new Chain(phrasesWithIndex);
-
 const getPiano = samples => getSampler(samples['vsco2-piano-mf']);
 
-const makePiece = ({ destination, audioContext, samples }) => {
-  if (audioContext !== Tone.context) {
-    Tone.setContext(audioContext);
+const activate = ({ destination, samples }) => {
+  const notes = instructions.tracks[1].notes.slice(0);
+  const eighthNotes = [];
+
+  for (let time = 0; time <= SONG_LENGTH; time += EIGHTH_NOTE_INTERVAL_S) {
+    const names = notes
+      .filter(
+        note => time <= note.time && note.time < time + EIGHTH_NOTE_INTERVAL_S
+      )
+      .map(({ name }) => name)
+      .sort();
+    eighthNotes.push(names.join(DELIMITER));
   }
+
+  const phrases = [];
+  const phraseLength = 32;
+  const enCopy = eighthNotes.slice(0);
+  while (enCopy.length > 0) {
+    phrases.push(enCopy.splice(0, phraseLength));
+  }
+
+  const phrasesWithIndex = phrases.map(phrase =>
+    phrase.map((names, i) =>
+      names.length === 0 ? `${i}` : `${i}${DELIMITER}${names}`
+    )
+  );
+
+  const chain = new Chain(phrasesWithIndex);
+
   return getPiano(samples).then(piano => {
     piano.connect(destination);
+
     const schedule = () => {
-      const phrase = chain.walk();
-      phrase.forEach(str => {
-        const [t, ...names] = str.split(DELIMITER);
-        const parsedT = Number.parseInt(t, 10);
-        names.forEach(name => {
-          const waitTime = parsedT * EIGHTH_NOTE_INTERVAL_S;
-          piano.triggerAttack(name, `+${waitTime + 1}`);
+      const schedulePhrase = () => {
+        const phrase = chain.walk();
+        phrase.forEach(str => {
+          const [t, ...names] = str.split(DELIMITER);
+          const parsedT = Number.parseInt(t, 10);
+          names.forEach(name => {
+            const waitTime = parsedT * EIGHTH_NOTE_INTERVAL_S;
+            piano.triggerAttack(
+              name,
+              `+${waitTime + 1 + Math.random() * 0.05 - 0.025}`
+            );
+          });
         });
-      });
+      };
+      Tone.Transport.scheduleRepeat(
+        schedulePhrase,
+        phraseLength * EIGHTH_NOTE_INTERVAL_S
+      );
     };
-    Tone.Transport.scheduleRepeat(
-      schedule,
-      phraseLength * EIGHTH_NOTE_INTERVAL_S
-    );
-    return () => {
+
+    const deactivate = () => {
       piano.dispose();
     };
+
+    return [deactivate, schedule];
   });
 };
 
-export default makePiece;
+export default makePiece(activate);
