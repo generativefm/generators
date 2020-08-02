1f2f8487e41963416b2a8fc2c98657064cb0fb73
metalex9 <alexbainter@gmail.com>
2020-08-02 09:40:06 -0500

    eno-machine: update to new interface

--- packages/piece-eno-machine/src/piece.js
+++ packages/piece-eno-machine/src/piece.js
@@ -1,7 +1,8 @@
 import { Chord, Array } from 'tonal';
-import randomNumber from 'random-number';
-import Tone from 'tone';
-import { getSampler } from '@generative-music/utilities';
+import * as Tone from 'tone';
+import { getSampler, makePiece } from '@generative-music/utilities';
+
+const randomNumber = ({ min, max }) => Math.random() * (max - min) + min;
 
 const OCTAVES = [3, 4, 5];
 const MIN_REPEAT_S = 20;
@@ -15,25 +16,28 @@ const NOTES = Array.rotate(1, Chord.notes('DbM9')).reduce(
 
 const getPiano = samples => getSampler(samples['vsco2-piano-mf']);
 
-const makePiece = ({ audioContext, destination, samples }) => {
-  if (Tone.context !== audioContext) {
-    Tone.setContext(audioContext);
-  }
+const activate = ({ destination, samples }) => {
   return getPiano(samples).then(piano => {
     piano.connect(destination);
-    NOTES.forEach(note => {
-      const interval = randomNumber({ min: MIN_REPEAT_S, max: MAX_REPEAT_S });
-      const delay = randomNumber({
-        min: 0,
-        max: MAX_REPEAT_S - MIN_REPEAT_S,
+
+    const schedule = () => {
+      NOTES.forEach(note => {
+        const interval = randomNumber({ min: MIN_REPEAT_S, max: MAX_REPEAT_S });
+        const delay = randomNumber({
+          min: 0,
+          max: MAX_REPEAT_S - MIN_REPEAT_S,
+        });
+        const playNote = () => piano.triggerAttack(note, '+1');
+        Tone.Transport.scheduleRepeat(playNote, interval, `+${delay}`);
       });
-      const playNote = () => piano.triggerAttack(note, '+1');
-      Tone.Transport.scheduleRepeat(playNote, interval, `+${delay}`);
-    });
-    return () => {
+    };
+
+    const deactivate = () => {
       piano.dispose();
     };
+
+    return [deactivate, schedule];
   });
 };
 
-export default makePiece;
+export default makePiece(activate);
