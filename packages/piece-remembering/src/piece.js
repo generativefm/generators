5ba146cc6680a2f04c9ad0535b1dbc403b7f5577
metalex9 <alexbainter@gmail.com>
2020-10-07 11:27:53 -0500

    Remembering: Update to v4

--- packages/piece-remembering/src/piece.js
+++ packages/piece-remembering/src/piece.js
@@ -1,11 +1,16 @@
-import Tone from 'tone';
-import { getSampler } from '@generative-music/utilities';
+import * as Tone from 'tone';
+import {
+  wrapActivate,
+  createPrerenderedSampler,
+  toss,
+} from '@generative-music/utilities';
+import { sampleNames } from '../remembering.gfm.manifest.json';
 
 const PITCH_CLASSES = ['C', 'D', 'E', 'G', 'A', 'C'];
 const getPhrase = octave => {
-  const notes = PITCH_CLASSES.map(pc => `${pc}${octave}`).concat([
-    `${PITCH_CLASSES[0]}${octave + 1}`,
-  ]);
+  const notes = toss(PITCH_CLASSES, [octave])
+    .concat()
+    .concat([`${PITCH_CLASSES[0]}${octave + 1}`]);
   return Array.from({ length: 4 }).map(
     () => notes[Math.floor(Math.random() * notes.length)]
   );
@@ -14,39 +19,55 @@ const getPhrase = octave => {
 const getPhrases = (octaves = [3, 4, 5, 6]) =>
   octaves.map(octave => getPhrase(octave));
 
-const makePiece = ({ audioContext, destination, samples }) => {
-  if (Tone.context !== audioContext) {
-    Tone.setContext(audioContext);
-  }
-  return getSampler(samples['vsco2-piano-mf']).then(piano => {
-    const reverb = new Tone.Freeverb(0.5)
-      .set({ wet: 0.5 })
-      .connect(destination);
-    piano.connect(reverb);
-
-    const playPhrase = () => {
-      const phrases = getPhrases();
-      const divisor = Math.random() * 0.15 + 0.5;
-      phrases.forEach(phrase =>
-        phrase.forEach((note, i) => {
-          if (Math.random() < 0.85) {
-            piano.triggerAttack(
-              note,
-              `+${i / divisor + Math.random() / 5 - 0.1}`
-            );
-          }
-        })
-      );
-
-      Tone.Transport.scheduleOnce(() => {
-        playPhrase();
-      }, `+${phrases[0].length / divisor + Math.random() * 5 + 3}`);
-    };
-    playPhrase();
-    return () => {
-      [piano, reverb].forEach(node => node.dispose());
-    };
+const activate = async ({ destination, sampleLibrary, onProgress }) => {
+  const samples = await sampleLibrary.request(Tone.context, sampleNames);
+  const piano = await createPrerenderedSampler({
+    samples,
+    sampleLibrary,
+    onProgress,
+    sourceInstrumentName: 'vsco2-piano-mf',
+    renderedInstrumentName: 'remembering::vsco2-piano-mf',
+    notes: toss(
+      PITCH_CLASSES.slice(0, -1).filter(pc => pc !== 'D' && pc !== 'A'),
+      [3, 4, 5, 6]
+    ).concat(['C7']),
+    getDestination: () =>
+      Promise.resolve(new Tone.Freeverb(0.5).set({ wet: 0.5 }).toDestination()),
   });
+  piano.connect(destination);
+
+  const playPhrase = () => {
+    const phrases = getPhrases();
+    const divisor = Math.random() * 0.15 + 0.5;
+    phrases.forEach(phrase =>
+      phrase.forEach((note, i) => {
+        if (Math.random() < 0.85) {
+          piano.triggerAttack(
+            note,
+            `+${i / divisor + Math.random() / 5 - 0.1}`
+          );
+        }
+      })
+    );
+
+    Tone.Transport.scheduleOnce(() => {
+      playPhrase();
+    }, `+${phrases[0].length / divisor + Math.random() * 5 + 3}`);
+  };
+
+  const schedule = () => {
+    playPhrase();
+
+    return () => {
+      piano.releaseAll(0);
+    };
+  };
+
+  const deactivate = () => {
+    piano.dispose();
+  };
+
+  return [deactivate, schedule];
 };
 
-export default makePiece;
+export default wrapActivate(activate);
