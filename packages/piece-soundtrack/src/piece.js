13ace5b1ccfe6cedb18b87dddd2ef6d95c3e60f4
metalex9 <alexbainter@gmail.com>
2020-10-07 10:55:47 -0500

    Soundtrack: Update to v4

--- packages/piece-soundtrack/src/piece.js
+++ packages/piece-soundtrack/src/piece.js
@@ -1,106 +1,96 @@
-import Tone from 'tone';
-import { Distance, Note } from 'tonal';
-import { getBuffers } from '@generative-music/utilities';
-
-const findClosest = (note, samplesByNote) => {
-  const noteMidi = Note.midi(note);
-  const maxInterval = 96;
-  let interval = 0;
-  while (interval <= maxInterval) {
-    const higherNote = Note.fromMidi(noteMidi + interval);
-    if (samplesByNote[higherNote]) {
-      return higherNote;
-    }
-    const lowerNote = Note.fromMidi(noteMidi - interval);
-    if (samplesByNote[lowerNote]) {
-      return lowerNote;
-    }
-    interval += 1;
-  }
-  return note;
-};
-
-const getCustomSampler = (
-  destination,
-  samplesByNote,
-  semitoneChange = 24,
-  offset = 0
-) => {
-  const activeSources = [];
-  return getBuffers(samplesByNote).then(buffers => ({
-    triggerAttack: (note, time = Tone.now()) => {
-      const closestSample = findClosest(note, samplesByNote);
-      const difference = Distance.semitones(closestSample, note);
-      const buffer = buffers.get(closestSample);
-      const playbackRate = Tone.intervalToFrequencyRatio(
-        difference - semitoneChange + Math.random() * 0.1 - 0.05
-      );
-      const bufferSource = new Tone.BufferSource(buffer)
-        .set({
-          playbackRate,
-          onended: () => {
-            const i = activeSources.indexOf(bufferSource);
-            if (i >= 0) {
-              activeSources.splice(i, 1);
-            }
-          },
-        })
-        .connect(destination);
-      activeSources.push(bufferSource);
-      bufferSource.start(time, offset);
-    },
-    dispose: () => {
-      [buffers, ...activeSources].forEach(node => node.dispose());
-    },
-  }));
-};
+import * as Tone from 'tone';
+import {
+  wrapActivate,
+  createPrerenderedSampler,
+  getRandomElement,
+  toss,
+} from '@generative-music/utilities';
+import { sampleNames } from '../soundtrack.gfm.manifest.json';
 
 const SECOND_NOTES = ['D', 'Eb', 'F', 'G', 'A'];
 const OCTAVES = [2, 3, 4];
 
-const makePiece = ({ audioContext, destination, samples }) => {
-  if (Tone.context !== audioContext) {
-    Tone.setContext(audioContext);
-  }
-  const reverb = new Tone.Reverb(50).connect(destination);
-  const glockDelay = new Tone.PingPongDelay(0.7, 0.7)
-    .set({ wet: 0.4 })
-    .connect(reverb);
-  return Promise.all([
-    getCustomSampler(reverb, samples['vsco2-cellos-susvib-mp']),
-    getCustomSampler(glockDelay, samples['vsco2-glock'], 36, 0.05),
-    reverb.generate(),
-  ]).then(([cellos, glock]) => {
-    const playProgression = () => {
-      const secondNote =
-        SECOND_NOTES[Math.floor(Math.random() * SECOND_NOTES.length)];
-      const secondNoteTime = Math.random() * 10 + 10 + 1;
-      OCTAVES.forEach(octave => {
-        cellos.triggerAttack(`C${octave}`, '+1');
-        if (Math.random() < 0.75) {
-          glock.triggerAttack(
-            `C${Math.random() < 0.5 ? 5 : 6}`,
-            `+${1 + Math.random() * secondNoteTime}`
-          );
-        }
-        cellos.triggerAttack(`${secondNote}${octave}`, `+${secondNoteTime}`);
-        if (Math.random() < 0.75) {
-          glock.triggerAttack(
-            `${secondNote}${Math.random() < 0.5 ? 5 : 6}`,
-            `+${secondNoteTime + Math.random() * 10}`
-          );
-        }
-      });
-      Tone.Transport.scheduleOnce(() => {
-        playProgression();
-      }, `+${Math.random() * 20 + 30}`);
-    };
+const activate = async ({ destination, sampleLibrary, onProgress }) => {
+  const samples = await sampleLibrary.request(Tone.context, sampleNames);
+  const getReverb = () => new Tone.Reverb(50).toDestination().generate();
+  const renderedPitchClasses = ['C'].concat(
+    SECOND_NOTES.filter((_, i) => i % 2 !== 0)
+  );
+
+  const cellos = await createPrerenderedSampler({
+    samples,
+    sampleLibrary,
+    notes: toss(renderedPitchClasses, OCTAVES),
+    sourceInstrumentName: 'vsco2-cellos-susvib-mp',
+    renderedInstrumentName: 'soundtrack::vsco2-cellos-susvib-mp',
+    getDestination: getReverb,
+    onProgress: val => onProgress(val * 0.5),
+    pitchShift: -24,
+  });
+
+  cellos.connect(destination);
+
+  const glock = await createPrerenderedSampler({
+    samples,
+    sampleLibrary,
+    notes: toss(renderedPitchClasses, [5, 6]),
+    sourceInstrumentName: 'vsco2-glock',
+    renderedInstrumentName: 'soundtrack::vsco2-glock',
+    getDestination: getReverb,
+    onProgress: val => onProgress(val * 0.5 + 0.5),
+    pitchShift: -36,
+  });
+
+  glock.set({
+    attack: 0.05,
+    curve: 'exponential',
+  });
+
+  const playProgression = () => {
+    const secondNote = getRandomElement(SECOND_NOTES);
+    const secondNoteTime = Math.random() * 10 + 10 + 1;
+    OCTAVES.forEach(octave => {
+      cellos.triggerAttack(`C${octave}`, '+1');
+      if (Math.random() < 0.75) {
+        glock.triggerAttack(
+          `C${Math.random() < 0.5 ? 5 : 6}`,
+          `+${1 + Math.random() * secondNoteTime}`
+        );
+      }
+      cellos.triggerAttack(`${secondNote}${octave}`, `+${secondNoteTime}`);
+      if (Math.random() < 0.75) {
+        glock.triggerAttack(
+          `${secondNote}${Math.random() < 0.5 ? 5 : 6}`,
+          `+${secondNoteTime + Math.random() * 10}`
+        );
+      }
+    });
+    Tone.Transport.scheduleOnce(() => {
+      playProgression();
+    }, `+${Math.random() * 20 + 30}`);
+  };
+
+  const schedule = () => {
+    const glockDelay = new Tone.PingPongDelay(0.7, 0.7)
+      .set({ wet: 0.4 })
+      .connect(destination);
+
+    glock.connect(glockDelay);
+
     playProgression();
 
     return () => {
-      [cellos, glock, reverb, glockDelay].forEach(node => node.dispose());
+      glock.releaseAll(0);
+      cellos.releaseAll(0);
+      glockDelay.dispose();
     };
-  });
+  };
+
+  const deactivate = () => {
+    [cellos, glock].forEach(node => node.dispose());
+  };
+
+  return [deactivate, schedule];
 };
 
-export default makePiece;
+export default wrapActivate(activate);
