2f4b96826b6c431fb9542af9cfe1dafa1cb0016e
metalex9 <alexbainter@gmail.com>
2020-10-06 21:46:26 -0500

    Western Medicine: Update to v4

--- packages/piece-western-medicine/src/piece.js
+++ packages/piece-western-medicine/src/piece.js
@@ -1,69 +1,47 @@
-import Tone from 'tone';
-import { Distance, Note } from 'tonal';
-import { getBuffers } from '@generative-music/utilities';
+import * as Tone from 'tone';
+import {
+  createPrerenderedSampledBuffers,
+  toss,
+  wrapActivate,
+} from '@generative-music/utilities';
+import { sampleNames } from '../western-medicine.gfm.manifest.json';
 
-const toss = (pcs = [], octaves = []) =>
-  octaves.reduce(
-    (notes, octave) => notes.concat(pcs.map(pc => `${pc}${octave}`)),
-    []
-  );
+const getCustomSampler = async ({ prerenderOptions, destination }) => {
+  const buffers = await createPrerenderedSampledBuffers(prerenderOptions);
 
-const findClosest = (note, samplesByNote) => {
-  const noteMidi = Note.midi(note);
-  const maxInterval = 96;
-  let interval = 0;
-  while (interval <= maxInterval) {
-    const higherNote = Note.fromMidi(noteMidi + interval);
-    if (samplesByNote[higherNote]) {
-      return higherNote;
-    }
-    const lowerNote = Note.fromMidi(noteMidi - interval);
-    if (samplesByNote[lowerNote]) {
-      return lowerNote;
-    }
-    interval += 1;
-  }
-  return note;
-};
-
-const getCustomSampler = (samplesByNote, destination) => {
   const activeSources = [];
-  const reverb = new Tone.Reverb(15);
-  const reverbWetLfo = new Tone.LFO(Math.random() / 100 + 0.01, 0.25, 1);
-  reverbWetLfo.connect(reverb.wet);
-  reverbWetLfo.start();
-  reverb.connect(destination);
-  return Promise.all([getBuffers(samplesByNote), reverb.generate()]).then(
-    ([buffers]) => ({
-      triggerAttack: (note, time = Tone.now()) => {
-        const closestSample = findClosest(note, samplesByNote);
-        const difference = Distance.semitones(closestSample, note);
-        const buffer = buffers.get(closestSample);
-        buffer.reverse = Math.random() < 0.5;
-        const playbackRate = Tone.intervalToFrequencyRatio(
-          difference - 24 + Math.random() * 0.1 - 0.05
-        );
-        const bufferSource = new Tone.BufferSource(buffer)
-          .set({
-            playbackRate,
-            onended: () => {
-              const i = activeSources.indexOf(bufferSource);
-              if (i >= 0) {
-                activeSources.splice(i, 1);
-              }
-            },
-          })
-          .connect(reverb);
-        activeSources.push(bufferSource);
-        bufferSource.start(time);
-      },
-      dispose: () => {
-        [buffers, reverb, reverbWetLfo, ...activeSources].forEach(node =>
-          node.dispose()
-        );
-      },
-    })
-  );
+  const triggerAttack = (note, time = Tone.now()) => {
+    const buffer = buffers.get(note);
+    buffer.reverse = Math.random() < 0.5;
+    const bufferSource = new Tone.BufferSource(buffer)
+      .set({
+        onended: () => {
+          const i = activeSources.indexOf(bufferSource);
+          if (i >= 0) {
+            activeSources.splice(i, 1);
+          }
+        },
+      })
+      .connect(destination);
+    activeSources.push(bufferSource);
+    bufferSource.start(time);
+  };
+
+  const releaseAll = time => {
+    activeSources.forEach(source => {
+      source.stop(time);
+    });
+  };
+
+  const dispose = () => {
+    [buffers, ...activeSources].forEach(node => node.dispose());
+  };
+
+  return {
+    triggerAttack,
+    releaseAll,
+    dispose,
+  };
 };
 
 const HARMONICS_NOTES = ['C4', 'G4', 'C3', 'C5'];
@@ -71,73 +49,105 @@ const MARIMBA_OCTS = [2, 3, 4, 5, 6];
 const HARP_NOTES = ['C4', 'G4', 'C5', 'G5', 'C6', 'G6'];
 const PIANO_NOTES = toss(['C', 'E', 'G'], [3, 4, 5, 6]);
 
-const makePiece = ({ audioContext, destination, samples }) => {
-  if (Tone.context !== audioContext) {
-    Tone.setContext(audioContext);
-  }
+const activate = async ({ destination, sampleLibrary, onProgress }) => {
+  const samples = await sampleLibrary.request(Tone.context, sampleNames);
   const filter = new Tone.Filter(3000).connect(destination);
-  return Promise.all(
-    ['guitar-harmonics', 'vsco2-marimba', 'vsco2-harp', 'vsco2-piano-mf'].map(
-      instrumentName => getCustomSampler(samples[instrumentName], filter)
+
+  const [harmonics, marimba, harp, piano] = await Promise.all(
+    [
+      ['guitar-harmonics', HARMONICS_NOTES],
+      ['vsco2-marimba', toss(['C', 'G'], MARIMBA_OCTS)],
+      ['vsco2-harp', HARP_NOTES],
+      ['vsco2-piano-mf', PIANO_NOTES],
+    ].map(([instrumentName, notes], i) =>
+      getCustomSampler({
+        prerenderOptions: {
+          samples,
+          sampleLibrary,
+          notes,
+          pitchShift: -24,
+          sourceInstrumentName: instrumentName,
+          renderedInstrumentName: `western-medicine::${instrumentName}`,
+          getDestination: () =>
+            new Tone.Reverb(15)
+              .set({ wet: 0.625 })
+              .toDestination()
+              .generate(),
+          onProgress: val => onProgress(val * 0.25 + 0.25 * i),
+          additionalRenderLength: 0.5,
+        },
+        destination: filter,
+      })
     )
-  ).then(([harmonics, marimba, harp, piano]) => {
-    const playHarmonics = () => {
-      const note =
-        HARMONICS_NOTES[Math.floor(Math.random() * HARMONICS_NOTES.length)];
-      harmonics.triggerAttack(note, '+1');
-      Tone.Transport.scheduleOnce(() => {
-        playHarmonics();
-      }, `+${Math.random() * 20 + 5}`);
-    };
+  );
+
+  const playHarmonics = () => {
+    const note =
+      HARMONICS_NOTES[Math.floor(Math.random() * HARMONICS_NOTES.length)];
+    harmonics.triggerAttack(note, '+1');
+    Tone.Transport.scheduleOnce(() => {
+      playHarmonics();
+    }, `+${Math.random() * 20 + 5}`);
+  };
+
+  const playMarimba = () => {
+    const notes = ['C', 'G'].map(
+      note =>
+        `${note}${
+          MARIMBA_OCTS[Math.floor(Math.random() * MARIMBA_OCTS.length)]
+        }`
+    );
+    notes.forEach(note => {
+      marimba.triggerAttack(note, `+${Math.random() * 0.2 + 1}`);
+    });
+    Tone.Transport.scheduleOnce(() => {
+      playMarimba();
+    }, `+${Math.random() * 20 + 20}`);
+  };
+
+  const playHarp = () => {
+    const note = HARP_NOTES[Math.floor(Math.random() * HARP_NOTES.length)];
+
+    harp.triggerAttack(note, '+1');
+    Tone.Transport.scheduleOnce(() => {
+      playHarp();
+    }, `+${Math.random() * 10 + 10}`);
+  };
+
+  piano.triggerAttack('C3');
+  const playPiano = () => {
+    const notes = Array.from({
+      length: Math.ceil(Math.random() * 5),
+    }).map(() => PIANO_NOTES[Math.floor(Math.random() * PIANO_NOTES.length)]);
+    const maxOffset = Math.random() * 3;
+
+    notes.forEach(note => {
+      piano.triggerAttack(note, `+${1 + Math.random() * maxOffset}`);
+    });
+
+    Tone.Transport.scheduleOnce(() => {
+      playPiano();
+    }, `+${Math.random() * 15 + 15}`);
+  };
+
+  const schedule = () => {
     playHarmonics();
-
-    const playMarimba = () => {
-      const notes = ['C', 'G'].map(
-        note =>
-          `${note}${
-            MARIMBA_OCTS[Math.floor(Math.random() * MARIMBA_OCTS.length)]
-          }`
-      );
-      notes.forEach(note => {
-        marimba.triggerAttack(note, `+${Math.random() * 0.2 + 1}`);
-      });
-      Tone.Transport.scheduleOnce(() => {
-        playMarimba();
-      }, `+${Math.random() * 20 + 20}`);
-    };
     playMarimba();
-
-    const playHarp = () => {
-      const note = HARP_NOTES[Math.floor(Math.random() * HARP_NOTES.length)];
-
-      harp.triggerAttack(note, '+1');
-      Tone.Transport.scheduleOnce(() => {
-        playHarp();
-      }, `+${Math.random() * 10 + 10}`);
-    };
     playHarp();
-
-    piano.triggerAttack('C3');
-    const playPiano = () => {
-      const notes = Array.from({
-        length: Math.ceil(Math.random() * 5),
-      }).map(() => PIANO_NOTES[Math.floor(Math.random() * PIANO_NOTES.length)]);
-      const maxOffset = Math.random() * 3;
-
-      notes.forEach(note => {
-        piano.triggerAttack(note, `+${1 + Math.random() * maxOffset}`);
-      });
-
-      Tone.Transport.scheduleOnce(() => {
-        playPiano();
-      }, `+${Math.random() * 15 + 15}`);
-    };
     playPiano();
 
     return () => {
-      [harmonics, marimba, harp, piano, filter].forEach(node => node.dispose());
+      [harmonics, marimba, harp, piano].forEach(sampler => {
+        sampler.releaseAll(0);
+      });
     };
-  });
+  };
+
+  const deactivate = () => {
+    [harmonics, marimba, harp, piano, filter].forEach(node => node.dispose());
+  };
+
+  return [deactivate, schedule];
 };
 
-export default makePiece;
+export default wrapActivate(activate);
