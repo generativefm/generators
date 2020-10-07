f10273588553449e50073ccb8b2ace5518578603
metalex9 <alexbainter@gmail.com>
2020-10-07 12:03:50 -0500

    Utilities: Fix transpose to account for implicit octave changes

--- packages/utilities/src/theory/transpose-note.js
+++ packages/utilities/src/theory/transpose-note.js
@@ -1,9 +1,25 @@
 import transposePitchClass from './transpose-pitch-class';
+import pitchClassIndiciesByValue from './pitch-class-indicies-by-value';
+
+const getImplicitOctaveChange = (pitchClassA, pitchClassB, wasTransposedUp) => {
+  const [indexA, indexB] = [pitchClassA, pitchClassB].map(
+    pc => pitchClassIndiciesByValue[pc]
+  );
+  if (wasTransposedUp && indexA > indexB) {
+    return 1;
+  } else if (!wasTransposedUp && indexA < indexB) {
+    return -1;
+  }
+  return 0;
+};
 
 const transposeNote = (pitchClass, octave, semitones) => {
   const nextPitchClass = transposePitchClass(pitchClass, semitones);
-  const octaveChange = Number.parseInt(semitones / 12, 10);
-  const nextOctave = octave + octaveChange;
+  const fullOctaveChange = Number.parseInt(semitones / 12, 10);
+  const nextOctave =
+    octave +
+    fullOctaveChange +
+    getImplicitOctaveChange(pitchClass, nextPitchClass, semitones > 0);
   return `${nextPitchClass}${nextOctave}`;
 };
 
