99628e23ae2330b86a10ed1e7d2679e5a33629b0
metalex9 <alexbainter@gmail.com>
2020-10-08 11:48:22 -0500

    420hz: update to v4

--- packages/piece-420hz-gamma-waves-for-big-brain/src/piece.js
+++ packages/piece-420hz-gamma-waves-for-big-brain/src/piece.js
@@ -1,77 +1,24 @@
-import Tone from 'tone';
-import { getBuffer, getBuffers } from '@generative-music/utilities';
-
-const pitchShiftSampler = (samplesByNote, destination, semitoneChange = 0) => {
-  const midiNoteMap = new Map(
-    Reflect.ownKeys(samplesByNote).map(note => [
-      new Tone.Midi(note).toMidi(),
-      note,
-    ])
-  );
-  const activeSources = [];
-  return getBuffers(samplesByNote).then(buffers => ({
-    play(note, time) {
-      const midi = new Tone.Midi(note);
-      let buffer;
-      let interval;
-      for (let i = 0; !buffer && i < 24; i += 1) {
-        //eslint-disable-next-line no-loop-func
-        [i, -i].some(transposition => {
-          const transposedMidi = midi.transpose(transposition);
-          if (midiNoteMap.has(transposedMidi.toMidi())) {
-            buffer = buffers.get(transposedMidi.toNote());
-            interval = -transposition;
-            return true;
-          }
-          return false;
-        });
-      }
-      if (buffer) {
-        const playbackRate = Tone.intervalToFrequencyRatio(
-          interval + semitoneChange
-        );
-        const source = new Tone.BufferSource(buffer).set({
-          playbackRate,
-          onended: () => {
-            const i = activeSources.indexOf(buffer);
-            if (i >= 0) {
-              activeSources.splice(i, 1);
-            }
-          },
-        });
-        source.connect(destination);
-        source.start(time);
-      }
-    },
-    dispose: () => {
-      [buffers, ...activeSources].forEach(node => node.dispose());
-      activeSources.splice(0, activeSources.length);
-    },
-  }));
-};
+import * as Tone from 'tone';
+import {
+  createBuffer,
+  createPitchShiftedSampler,
+  wrapActivate,
+  getRandomElement,
+  getPitchClass,
+  getOctave,
+} from '@generative-music/utilities';
+import { sampleNames } from '../420hz-gamma-waves-for-big-brain.gfm.manifest.json';
 
 const getRandomPhase = () => Math.random() * 360;
 
-const makePiece = ({ audioContext, destination, samples }) => {
-  if (Tone.context !== audioContext) {
-    Tone.setContext(audioContext);
-  }
+const MAX_DRONE_GAIN = 0.4;
+
+const activate = async ({ destination, sampleLibrary }) => {
+  const samples = await sampleLibrary.request(Tone.context, sampleNames);
   const stereoWidener = new Tone.StereoWidener();
   const crossFade = new Tone.CrossFade().connect(stereoWidener);
-  const crossFadeLfo = new Tone.LFO(Math.random() / 10000 + 0.0001).set({
-    phase: 90,
-  });
-  crossFadeLfo.connect(crossFade.fade);
-  crossFadeLfo.start();
-  const stereoLfo = new Tone.LFO(Math.random() / 100 + 0.01, 0.5, 1).set({
-    phase: getRandomPhase(),
-  });
-  stereoLfo.connect(stereoWidener.width);
-  stereoLfo.start();
-  const autoLowPass = new Tone.AutoFilter(Math.random() / 100 + 0.01, 100, 1)
-    .set({ filter: { rolloff: -96 } })
-    .connect(crossFade, 0, 0);
-  autoLowPass.start();
+  const pianoGain = new Tone.Gain().connect(stereoWidener);
+
   const chords = [
     ['G3', 'C4', 'E4'],
     ['E4', 'A4', 'C5'],
@@ -79,127 +26,180 @@ const makePiece = ({ audioContext, destination, samples }) => {
     ['C4', 'F4', 'G4'],
   ];
 
-  const pianoVolLfo = new Tone.LFO(Math.random() / 10000 + 0.0001, -100, 0).set(
-    {
-      phase: getRandomPhase(),
-    }
-  );
-  const pianoVol = new Tone.Volume().connect(stereoWidener);
-  pianoVolLfo.connect(pianoVol.volume);
-  pianoVolLfo.start();
-
-  return Promise.all([
-    getBuffer(samples.waves[0]),
-    pitchShiftSampler(samples['vsco2-piano-mf'], pianoVol, -24),
-    ...chords.map(chord => {
-      const volume = new Tone.Volume(-100).connect(autoLowPass);
-      return pitchShiftSampler(samples['sso-chorus-male'], volume, -24).then(
-        sampler => [chord, volume, sampler]
-      );
+  const [waveBuffer, pitchShiftPiano, ...droneStacks] = await Promise.all([
+    createBuffer(samples.waves[0]),
+    createPitchShiftedSampler({
+      samplesByNote: samples['vsco2-piano-mf'],
+      pitchShift: -24,
     }),
-  ]).then(([waveBuffer, pitchShiftPiano, ...droneStacks]) => {
-    const loopWaves = () => {
-      const playbackRate = 0.5;
-      const source = new Tone.BufferSource(waveBuffer)
-        .set({ fadeIn: 5, fadeOut: 5, curve: 'linear', playbackRate })
-        .connect(crossFade, 0, 1);
-      source.start('+1', 0, waveBuffer.duration / playbackRate - 5);
-
-      Tone.Transport.scheduleOnce(() => {
-        loopWaves();
-      }, `+${waveBuffer.duration / playbackRate - 5}`);
-    };
-
-    loopWaves();
-
-    const drone = (chorus, note, min, max) => {
-      chorus.play(note, '+1');
-
-      Tone.Transport.scheduleOnce(() => {
-        drone(chorus, note, min, max);
-      }, `+${Math.random() * (max - min) + min}`);
-    };
-    // eslint-disable-next-line no-unused-vars
-    droneStacks.forEach(([chord, volume, sampler]) => {
-      chord.forEach(note => {
-        drone(sampler, note, 20, 10);
+    ...chords.map(async chord => {
+      const gain = new Tone.Gain(0);
+      const sampler = await createPitchShiftedSampler({
+        samplesByNote: samples['sso-chorus-male'],
+        pitchShift: -24,
       });
+      sampler.connect(gain);
+      return [chord, gain, sampler];
+    }),
+  ]);
+
+  pitchShiftPiano.connect(pianoGain);
+
+  const activeSources = [];
+
+  const loopWaves = () => {
+    const playbackRate = 0.5;
+    const source = new Tone.BufferSource(waveBuffer)
+      .set({
+        fadeIn: 5,
+        fadeOut: 5,
+        curve: 'linear',
+        onended: () => {
+          const i = activeSources.indexOf(source);
+          if (i >= 0) {
+            activeSources.splice(i, 1);
+          }
+        },
+        playbackRate,
+      })
+      .connect(crossFade.b);
+    activeSources.push(source);
+    source.start('+1', 0, waveBuffer.duration / playbackRate - 5);
+
+    Tone.Transport.scheduleOnce(() => {
+      loopWaves();
+    }, `+${waveBuffer.duration / playbackRate - 5}`);
+  };
+
+  const drone = (chorus, note, min, max) => {
+    chorus.triggerAttack(note, '+1');
+
+    Tone.Transport.scheduleOnce(() => {
+      drone(chorus, note, min, max);
+    }, `+${Math.random() * (max - min) + min}`);
+  };
+
+  let unmutedDroneGain = getRandomElement(droneStacks)[1];
+  unmutedDroneGain.gain.setValueAtTime(MAX_DRONE_GAIN, Tone.now());
+
+  const changeChord = () => {
+    const transitionTime = Math.ceil(Math.random() * 10 + 10);
+    const mutedDroneStacks = droneStacks.filter(
+      ([, gain]) => gain !== unmutedDroneGain
+    );
+    const nextUnmutedDroneStack = getRandomElement(mutedDroneStacks);
+    unmutedDroneGain.gain.cancelScheduledValues(Tone.now());
+    unmutedDroneGain.gain.setValueAtTime(
+      unmutedDroneGain.gain.value,
+      Tone.now()
+    );
+    unmutedDroneGain.gain.linearRampToValueAtTime(0, `+${transitionTime}`);
+    unmutedDroneGain = nextUnmutedDroneStack[1];
+    unmutedDroneGain.gain.setValueAtTime(0, Tone.now());
+    unmutedDroneGain.gain.linearRampToValueAtTime(
+      MAX_DRONE_GAIN,
+      `+${transitionTime}`
+    );
+
+    const nextChordChangeDelay = Math.random() * 10 + 10;
+
+    const [unmutedDroneChord] = nextUnmutedDroneStack;
+
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const playChance = (minutes / 60) % 1;
+    const octaveChange = hours <= 6 || hours >= 18 ? 2 : 1;
+    unmutedDroneChord
+      .filter(() => Math.random() < playChance)
+      .forEach(note => {
+        const noteTime =
+          Math.random() * (nextChordChangeDelay + transitionTime / 2) +
+          transitionTime / 2;
+        const pc = getPitchClass(note);
+        const oct = getOctave(note);
+        pitchShiftPiano.triggerAttack(
+          `${pc}${oct + octaveChange}`,
+          `+${noteTime}`
+        );
+      });
+
+    Tone.Transport.scheduleOnce(() => {
+      changeChord();
+    }, `+${transitionTime + nextChordChangeDelay}`);
+  };
+
+  stereoWidener.connect(destination);
+
+  const schedule = () => {
+    const pianoGainLfo = new Tone.LFO(Math.random() / 10000 + 0.0001).set({
+      phase: getRandomPhase(),
     });
+    pianoGainLfo.connect(pianoGain.gain);
+    pianoGainLfo.start();
 
-    let unmutedDroneVolume =
-      droneStacks[Math.floor(Math.random() * droneStacks.length)][1];
-    unmutedDroneVolume.volume.value = -10;
+    const crossFadeLfo = new Tone.LFO(Math.random() / 10000 + 0.0001).set({
+      phase: 90,
+    });
+    crossFadeLfo.connect(crossFade.fade);
+    crossFadeLfo.start();
 
-    const changeChord = () => {
-      const transitionTime = Math.ceil(Math.random() * 10 + 10);
-      const mutedDroneStacks = droneStacks.filter(
-        // eslint-disable-next-line no-unused-vars
-        ([chord, volume]) => volume !== unmutedDroneVolume
-      );
-      const nextUnmutedDroneStack =
-        mutedDroneStacks[Math.floor(Math.random() * mutedDroneStacks.length)];
-      unmutedDroneVolume.volume.linearRampToValueAtTime(
-        -100,
-        `+${transitionTime}`
-      );
-      unmutedDroneVolume = nextUnmutedDroneStack[1];
-      unmutedDroneVolume.volume.value = -100;
-      unmutedDroneVolume.volume.linearRampToValueAtTime(
-        -10,
-        `+${transitionTime}`
-      );
+    const stereoLfo = new Tone.LFO(Math.random() / 100 + 0.01, 0.5, 1).set({
+      phase: getRandomPhase(),
+    });
+    stereoLfo.connect(stereoWidener.width);
+    stereoLfo.start();
 
-      const nextChordChangeDelay = Math.random() * 10 + 10;
-
-      const [unmutedDroneChord] = nextUnmutedDroneStack;
-
-      const date = new Date();
-      const minutes = date.getMinutes();
-      const hours = date.getHours();
-      const playChance = (minutes / 60) % 1;
-      const octaveChange = hours <= 6 || hours >= 18 ? 2 : 1;
-      unmutedDroneChord
-        .filter(() => Math.random() < playChance)
-        .forEach(note => {
-          const noteTime =
-            Math.random() * (nextChordChangeDelay + transitionTime / 2) +
-            transitionTime / 2;
-          const [pc, octStr] = note;
-          const oct = Number.parseInt(octStr, 10);
-          pitchShiftPiano.play(`${pc}${oct + octaveChange}`, `+${noteTime}`);
-        });
-
-      Tone.Transport.scheduleOnce(() => {
-        changeChord();
-      }, `+${transitionTime + nextChordChangeDelay}`);
-    };
+    const autoLowPass = new Tone.AutoFilter(Math.random() / 100 + 0.01, 100, 1)
+      .set({ filter: { rolloff: -96 } })
+      .connect(crossFade.a);
+    autoLowPass.start();
 
     Tone.Transport.scheduleOnce(() => {
       changeChord();
     }, `+${Math.random() * 10 + 10}`);
 
-    stereoWidener.connect(destination);
+    droneStacks.forEach(([chord, volume, sampler]) => {
+      volume.connect(autoLowPass);
+      chord.forEach(note => {
+        drone(sampler, note, 20, 10);
+      });
+    });
+
+    loopWaves();
 
     return () => {
-      [
-        stereoWidener,
-        crossFade,
-        crossFadeLfo,
-        stereoLfo,
-        autoLowPass,
-        pianoVolLfo,
-        pianoVol,
-        waveBuffer,
-        pitchShiftPiano,
-        ...droneStacks.reduce(
-          // eslint-disable-next-line no-unused-vars
-          (disposableNodes, [chord, volume, sampler]) =>
-            disposableNodes.concat([volume, sampler]),
-          []
-        ),
-      ].forEach(node => node.dispose());
+      activeSources.forEach(source => {
+        source.stop(0);
+      });
+      pitchShiftPiano.releaseAll(0);
+      droneStacks.forEach(([, gain, sampler]) => {
+        gain.cancelScheduledValues(Tone.now());
+        gain.setValueAtTime(0, Tone.now());
+        sampler.releaseAll(0);
+      });
+      [pianoGainLfo, crossFadeLfo, stereoLfo, autoLowPass].forEach(node => {
+        node.dispose();
+      });
     };
-  });
+  };
+
+  const deactivate = () => {
+    [
+      stereoWidener,
+      crossFade,
+      pianoGain,
+      waveBuffer,
+      pitchShiftPiano,
+      ...droneStacks.reduce(
+        (disposableNodes, [, gain, sampler]) =>
+          disposableNodes.concat([gain, sampler]),
+        []
+      ),
+    ].forEach(node => node.dispose());
+  };
+
+  return [deactivate, schedule];
 };
 
-export default makePiece;
+export default wrapActivate(activate);
