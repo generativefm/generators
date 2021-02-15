f594e4fb37cc2c0b759ffc1c243ff896b1d27d06
metalex9 <alexbainter@gmail.com>
2021-02-14 21:45:11 -0600

    Add bandcamp urls to manifest files

---
+++ scripts/add-bandcamp-urls.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const fsp = require('fs').promises;
+const glob = require('glob');
+const bandcampScraper = require('bandcamp-scraper');
+
+const getManifests = () =>
+  new Promise((resolve, reject) => {
+    glob('./packages/*/*.gfm.manifest.json', (err, files) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(files);
+    });
+  });
+
+const getAlbumUrls = () =>
+  new Promise((resolve, reject) => {
+    bandcampScraper.getAlbumUrls(
+      'http://alexbainter.bandcamp.com/',
+      (err, urls) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(urls);
+      }
+    );
+  });
+
+Promise.all([getManifests(), getAlbumUrls()]).then(
+  ([manifestPaths, albumUrls]) => {
+    const unmatchedUrls = albumUrls.slice();
+    const newManifests = manifestPaths.map(relativePath => {
+      const manifest = require(relativePath.replace('./', '../'));
+      const { id } = manifest;
+      const urlIndex = unmatchedUrls.findIndex(url =>
+        url.startsWith(`http://alexbainter.bandcamp.com/album/${id}-`)
+      );
+      const [url] = unmatchedUrls.splice(urlIndex, 1);
+      if (!url) {
+        throw relativePath;
+      }
+      return [relativePath, Object.assign({}, manifest, { bandcampUrl: url })];
+    });
+    return Promise.all(
+      newManifests.map(([relativePath, manifest]) =>
+        fsp.writeFile(relativePath, JSON.stringify(manifest, null, 2))
+      )
+    );
+  }
+);
