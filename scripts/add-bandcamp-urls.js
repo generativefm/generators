1b7dca52ce72d49150f5e0249ba3bee6daa38a6e
metalex9 <alexbainter@gmail.com>
2021-02-18 16:07:47 -0600

    Fix bandcamp urls

--- scripts/add-bandcamp-urls.js
+++ scripts/add-bandcamp-urls.js
@@ -31,19 +31,24 @@ const getAlbumUrls = () =>
 
 Promise.all([getManifests(), getAlbumUrls()]).then(
   ([manifestPaths, albumUrls]) => {
-    const unmatchedUrls = albumUrls.slice();
+    const unmatchedUrls = albumUrls.filter(
+      url => !url.includes('corruption-loops')
+    );
+    const unmatchedIds = [];
     const newManifests = manifestPaths.map(relativePath => {
       const manifest = require(relativePath.replace('./', '../'));
       const { id } = manifest;
       const urlIndex = unmatchedUrls.findIndex(url =>
-        url.startsWith(`http://alexbainter.bandcamp.com/album/${id}-`)
+        url.startsWith(`http://alexbainter.bandcamp.com/album/${id}-excerpts`)
       );
-      const [url] = unmatchedUrls.splice(urlIndex, 1);
-      if (!url) {
-        throw relativePath;
+      if (urlIndex === -1) {
+        unmatchedIds.push(id);
+        return [relativePath, Object.assign({}, manifest)];
       }
+      const [url] = unmatchedUrls.splice(urlIndex, 1);
       return [relativePath, Object.assign({}, manifest, { bandcampUrl: url })];
     });
+    console.log(unmatchedIds);
     return Promise.all(
       newManifests.map(([relativePath, manifest]) =>
         fsp.writeFile(relativePath, JSON.stringify(manifest, null, 2))
