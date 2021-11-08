83b9a15a7f4bf7d162202ac16e49888709b9ecdb
Alex Bainter <alexbainter@gmail.com>
2021-11-07 18:50:54 -0600

    Fix stuff

--- packages/piece-skyline/src/piece.js
+++ packages/piece-skyline/src/piece.js
@@ -1,17 +1,15 @@
 import * as Tone from 'tone';
 import {
   wrapActivate,
-  transpose,
   createPrerenderableBufferArray,
   createPrerenderableSampler,
-  createPitchShiftedSampler,
 } from '@generative-music/utilities';
 import { sampleNames, id } from '../piece.gfm.manifest.json';
 import gainAdjustments from '../../../normalize/gain.json';
 
 const BASE_TIME_UNIT = 0.54;
 
-const createPercussionInstrument = (buffer) => {
+const createPercussionInstrument = buffer => {
   const output = new Tone.Gain();
   const activeSources = [];
 
@@ -34,24 +32,27 @@ const createPercussionInstrument = (buffer) => {
     source.start(time);
   };
 
-  const releaseAll = (time) => {
-    activeSources.forEach((source) => {
+  const releaseAll = time => {
+    activeSources.forEach(source => {
       source.set({ fadeOut: 0 });
       source.stop(time);
     });
   };
 
   const dispose = () => {
-    activeSources.forEach((node) => {
+    activeSources.forEach(node => {
       node.stop();
     });
   };
 
-  return { connect, toDestination, triggerAttack, dispose, releaseAll };
+  return { connect, triggerAttack, dispose, releaseAll };
 };
 
 const getPercussionDestination = () =>
-  new Tone.Reverb(10).set({ wet: 0.15 }).generate();
+  new Tone.Reverb(10)
+    .set({ wet: 0.15 })
+    .toDestination()
+    .generate();
 
 const activate = async ({ sampleLibrary, onProgress }) => {
   const defaultSamples = await sampleLibrary.request(
@@ -62,11 +63,11 @@ const activate = async ({ sampleLibrary, onProgress }) => {
     'itslucid-lofi-hats': defaultSamples['itslucid-lofi-hats']
       ? [defaultSamples['itslucid-lofi-hats'][8]]
       : null,
-    'itslucid-lofi-kicks': defaultSamples['itslucid-lofi-kicks']
-      ? [defaultSamples['itslucid-lofi-kicks'][0]]
+    'itslucid-lofi-kick': defaultSamples['itslucid-lofi-kick']
+      ? [defaultSamples['itslucid-lofi-kick'][0]]
       : null,
-    'itslucid-lofi-snares': defaultSamples['itslucid-lofi-snares']
-      ? [defaultSamples['itslucid-lofi-snares'][10]]
+    'itslucid-lofi-snare': defaultSamples['itslucid-lofi-snare']
+      ? [defaultSamples['itslucid-lofi-snare'][10]]
       : null,
   });
   const [hatBuffer] = await createPrerenderableBufferArray({
@@ -76,25 +77,25 @@ const activate = async ({ sampleLibrary, onProgress }) => {
     sampleLibrary,
     getDestination: getPercussionDestination,
     additionalRenderLength: 0.5,
-    onProgress: (val) => onProgress(val * 0.25),
+    onProgress: val => onProgress(val * 0.25),
   });
   const [kickBuffer] = await createPrerenderableBufferArray({
     samples,
-    sourceInstrumentName: 'itslucid-lofi-kicks',
-    renderedInstrumentName: 'skyline__itslucid-lofi-kicks',
+    sourceInstrumentName: 'itslucid-lofi-kick',
+    renderedInstrumentName: 'skyline__itslucid-lofi-kick',
     sampleLibrary,
     getDestination: getPercussionDestination,
     additionalRenderLength: 0.5,
-    onProgress: (val) => onProgress(val * 0.25 + 0.25),
+    onProgress: val => onProgress(val * 0.25 + 0.25),
   });
   const [snareBuffer] = await createPrerenderableBufferArray({
     samples,
-    sourceInstrumentName: 'itslucid-lofi-snares',
-    renderedInstrumentName: 'skyline__itslucid-lofi-snares',
+    sourceInstrumentName: 'itslucid-lofi-snare',
+    renderedInstrumentName: 'skyline__itslucid-lofi-snare',
     sampleLibrary,
     getDestination: getPercussionDestination,
     additionalRenderLength: 0.5,
-    onProgress: (val) => onProgress(val * 0.25 + 0.5),
+    onProgress: val => onProgress(val * 0.25 + 0.5),
   });
   const violins = await createPrerenderableSampler({
     notes: ['C4', 'G4', 'G5'],
@@ -103,8 +104,12 @@ const activate = async ({ sampleLibrary, onProgress }) => {
     sourceInstrumentName: 'vsco2-violins-susvib',
     renderedInstrumentName: 'skyline__vsco2-violins-susvib',
     sampleLibrary,
-    getDestination: () => new Tone.Reverb(30).set({ wet: 0.5 }).generate(),
-    onProgress: (val) => onProgress(val * 0.25 + 0.75),
+    getDestination: () =>
+      new Tone.Reverb(30)
+        .set({ wet: 0.5 })
+        .toDestination()
+        .generate(),
+    onProgress: val => onProgress(val * 0.25 + 0.75),
   });
   const hats = createPercussionInstrument(hatBuffer);
   const kick = createPercussionInstrument(kickBuffer);
@@ -127,11 +132,11 @@ const activate = async ({ sampleLibrary, onProgress }) => {
     )
       .start()
       .connect(destination);
-    [hats, kick, snare].forEach((instrument) => {
+    [hats, kick, snare].forEach(instrument => {
       instrument.connect(destination);
     });
 
-    Tone.Transport.scheduleRepeat((time) => {
+    Tone.Transport.scheduleRepeat(time => {
       panner.pan.setValueAtTime(panner.pan.value, time + 1);
       panner.pan.linearRampToValueAtTime(Math.random() * 2 - 1, time + 3);
       panner.set({ pan: Math.random() * 2 - 1 });
@@ -144,29 +149,29 @@ const activate = async ({ sampleLibrary, onProgress }) => {
         sixteenthNoteIndex += 1
       ) {
         if (sixteenthNoteIndex % 2 === 0 || Math.random() < 0.1) {
-          hatSampler.triggerAttack(
+          hats.triggerAttack(
             time + 1 + (sixteenthNoteIndex * BASE_TIME_UNIT) / 4,
             0.03
           );
         }
       }
-      kickSampler.triggerAttack(time + 1, 0.25);
+      kick.triggerAttack(time + 1, 0.25);
       if (Math.random() < 0.2) {
-        kickSampler.triggerAttack(time + 1 + BASE_TIME_UNIT, 0.25);
+        kick.triggerAttack(time + 1 + BASE_TIME_UNIT, 0.25);
       }
       if (Math.random() < 0.2) {
-        kickSampler.triggerAttack(time + 1 + BASE_TIME_UNIT * 7, 0.25);
+        kick.triggerAttack(time + 1 + BASE_TIME_UNIT * 7, 0.25);
       }
       if (Math.random() < 0.1) {
         return;
       }
       if (Math.random() < 0.33) {
-        snareSampler.triggerAttack(time + 1 + 3.25 * BASE_TIME_UNIT, 0.2);
+        snare.triggerAttack(time + 1 + 3.25 * BASE_TIME_UNIT, 0.2);
       }
-      snareSampler.triggerAttack(time + 1 + 4 * BASE_TIME_UNIT, 0.2);
+      snare.triggerAttack(time + 1 + 4 * BASE_TIME_UNIT, 0.2);
       if (Math.random() < 0.05) {
-        snareSampler.triggerAttack(time + 1 + 7.5 * BASE_TIME_UNIT, 0.2);
-        snareSampler.triggerAttack(time + 1 + 7.75 * BASE_TIME_UNIT, 0.2);
+        snare.triggerAttack(time + 1 + 7.5 * BASE_TIME_UNIT, 0.2);
+        snare.triggerAttack(time + 1 + 7.75 * BASE_TIME_UNIT, 0.2);
       }
     }, BASE_TIME_UNIT * 8);
 
@@ -174,7 +179,7 @@ const activate = async ({ sampleLibrary, onProgress }) => {
     let lastExtraNoteTime = Tone.now();
 
     const drone = () => {
-      sampler.triggerAttack('C4');
+      violins.triggerAttack('C4');
 
       Tone.Transport.scheduleOnce(() => {
         drone();
@@ -186,39 +191,41 @@ const activate = async ({ sampleLibrary, onProgress }) => {
 
       lastExtraNote = Math.random() < 0.6 || lastExtraNote === 'A' ? 'G' : 'A';
       lastExtraNoteTime = Tone.now();
-      sampler.triggerAttack(`${lastExtraNote}4`);
+      violins.triggerAttack(`${lastExtraNote}4`);
       if (Math.random() < 0.2) {
-        sampler.triggerAttack(`${lastExtraNote}5`);
+        violins.triggerAttack(`${lastExtraNote}5`);
       }
     };
     drone();
 
     return () => {
       percussionAutoFilter.dispose();
-      [hats, kick, snare].forEach((instrument) => {
+      [hats, kick, snare].forEach(instrument => {
         instrument.releaseAll(0);
       });
     };
-
-    const deactivate = () => {
-      [
-        lowpass,
-        violins,
-        hats,
-        kick,
-        snare,
-        hatBuffer,
-        kickBuffer,
-        snareBuffer,
-        panner,
-        synth,
-      ].forEach((node) => {
-        node.dispose();
-      });
-    };
-
-    return [deactivate, schedule];
   };
+
+  const deactivate = () => {
+    [
+      lowpass,
+      violins,
+      hats,
+      kick,
+      snare,
+      hatBuffer,
+      kickBuffer,
+      snareBuffer,
+      panner,
+      synth,
+    ].forEach(node => {
+      node.dispose();
+    });
+  };
+
+  return [deactivate, schedule];
 };
+
+const GAIN_ADJUSTMENT = gainAdjustments[id] || 1;
 
 export default wrapActivate(activate, { gain: GAIN_ADJUSTMENT });
